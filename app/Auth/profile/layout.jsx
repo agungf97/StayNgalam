ef127dc6f44605f2b1d70cfc1a0c00da3160bea0
Mohamad Agung Faisal <agoenkf97@gmail.com>
2023-11-15 11:54:11 +0700

    fixed layout

--- app/Auth/profile/layout.jsx
+++ app/Auth/profile/layout.jsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../../globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
